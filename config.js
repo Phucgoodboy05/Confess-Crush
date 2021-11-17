38e7168af9b1f16197e7063cc18ba0febeb6b332
Phucgoodboy05 <94528387+Phucgoodboy05@users.noreply.github.com>
2021-11-17 17:37:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,12 @@
 const CONFIG = {
-    titleWeb: "Làm ny Phúc được không",
+    titleWeb: "20/11",
     introTitle: '',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Chúc thầy 20/11 vui vẻ`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Mình đồng ý',
-    btnNo: 'Tớ chỉ xem cậu là bạn',
+    btnYes: 'Thầy đồng ý',
+    btnNo: 'KHông có đâu em',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho Phúc <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
